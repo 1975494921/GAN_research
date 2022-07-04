59a73995fe161a093bde0e3ceb4e1b51ab36fde8
1975494921 <1975494921@qq.com>
2022-07-04 17:48:03 +0100

    parallel

--- benchmark.py
+++ benchmark.py
@@ -3,11 +3,11 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
 # Parameters and DataLoaders
-input_size = 3000
-output_size = 3000
+input_size = 20000
+output_size = 20000
 
-batch_size = 1000000
-data_size = 10000000
+batch_size = 50000
+data_size = 100000
 
 device_group_1 = [0, 1, 2, 3]
 device_group_2 = [1, 0, 2, 3]
@@ -17,7 +17,6 @@ device2 = torch.device("cuda:{}".format(device_group_2[0]))
 
 
 class RandomDataset(Dataset):
-
     def __init__(self, size, length):
         self.len = length
         self.data = torch.randn(length, size)
@@ -31,6 +30,8 @@ class RandomDataset(Dataset):
 
 rand_loader = DataLoader(dataset=RandomDataset(input_size, data_size),
                          batch_size=batch_size, shuffle=True)
+
+print("1")
 
 
 class Model(nn.Module):
@@ -46,22 +47,27 @@ class Model(nn.Module):
         return output
 
 
+print("2")
 model1 = Model(input_size, output_size)
 model2 = Model(output_size, output_size)
+print("3")
 if torch.cuda.device_count() > 1:
     print("Let's use", torch.cuda.device_count(), "GPUs!")
     model1 = nn.DataParallel(model1, device_ids=device_group_1)
     model2 = nn.DataParallel(model2, device_ids=device_group_2)
 
+print("4")
 model1.to(device1)
 model2.to(device2)
+print("5")
 optim1 = torch.optim.Adam(params=model1.parameters(), lr=0.001)
 optim2 = torch.optim.SGD(params=model2.parameters(), lr=0.001)
-
+print("6")
 loss_func = nn.MSELoss()
 
 input = torch.randn((batch_size, input_size)).to(device1)
 target = torch.randn((batch_size, output_size)).to(device2)
+print("7")
 print("Start training...")
 while True:
     output = model1(input)
