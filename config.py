cd0c5e71da9e4f0161b004d62f9025e78c65ea88
1975494921 <1975494921@qq.com>
2022-07-20 14:24:24 +0100

    PGAN 7.20

--- config.py
+++ config.py
@@ -1,18 +1,34 @@
 import torch
+from PGAN_utils import size_to_depth, depth_to_size
 
 
 class Config:
     debug = False
-
     ngpus = min(torch.cuda.device_count(), 8)
-    gpu_shift = 0
+    gpu_shift = 2
     device_groups = [[i for i in range(gpu_shift, ngpus + gpu_shift)],
                      [i for i in range(gpu_shift, ngpus + gpu_shift)]]
 
     devices = ["cuda:{}".format(item[0]) for item in device_groups]
+    model_root = 'model_trains'
 
-    nz_dim = 32  # Noise dim
+    Project_Params = dict()
+    Project_Params['cartoon_train001'] = {
+        'load_depth': 5,
+        'start_depth': 5,
+        'end_depth': size_to_depth(1024),
 
-    nz_side = 1
+        'Use_last_alpha': True,
+        'Use_Mean': True,
+        'G_lr': 0.0001,
+        'D_lr': 0.00005,
 
-    batch_size = 256
+        'data_dir': '/home/zceelil/dataset/cartoon/imsize_500/cartoonset100k',
+        'epos_list': [0, 300, 300, 480, 60, 60, 60, 60, 40, 40],
+        'batch_list': [0, 1000, 1000, 1000, 1000, 580, 200, 100, 80, 50],
+        'save_internal': [0, 20, 20, 20, 20, 5, 2, 1, 1, 1],
+        'alpha_list': [0, 0.01, 0.01, 0.01, 0.01, 1.00, 1.00, 1.00, 1.00, 0.00, 0.00],
+        'delta_alpha': 0.002,
+
+        'train_last_layer_only': False,
+    }
